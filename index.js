03c118cc6ff0e68ece9be42fef4bb77ca3471fa1
ishamsu <shamsu1997@gmail.com>
2020-06-06 22:50:57 +0530

    test

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ Issue Body : *${ibody}*
                     // Switch statement for Pull Requests
             case "pull_request":
                 return `
-Made a pull request ✅ ✅1
+Made a pull request ✅ ✅12
 
 PR ${prstate} 
 
