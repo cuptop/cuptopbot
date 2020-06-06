95e40cf92394513451a5f5cd8b1ae3d06de0e4ce
ishamsu <shamsu1997@gmail.com>
2020-06-06 19:36:48 +0530

    🦊 test-bot commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ Issue Body : *${ibody}*
                     // Switch statement for Pull Requests
             case "pull_request":
                 return `
-Made a pull request ✅ ✅
+Made a pull request ✅
 
 PR ${prstate} 
 
